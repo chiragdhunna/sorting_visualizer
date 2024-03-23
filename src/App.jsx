462b27e57cd971c386cfc3a9511a4745b53099f1
chiragdhunna <chiragdhunna2468@gmail.com>
2024-03-24 02:22:05 +0530

    Completing the bubble Sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Appbar from "./components/Appbar";
+import { FaArrowDown } from "react-icons/fa";
 
 function App() {
   let i = 0;
@@ -15,23 +16,24 @@ function App() {
   }
 
   function heightChange() {
+    let arrowDisplay = false;
     let newCont = [];
     let newArr = [];
     for (i = 0; i < 10; i++) {
       let h = Math.floor(Math.random() * (600 - 50 + 1) + 50);
       newArr.push(h);
       newCont.push(
-        <div>
-          <div></div>
+        <div id={i + 70}>
+          {/* {arrowDisplay && (
+            <div className="DownArrow">
+              <FaArrowDown />
+            </div>
+          )} */}
           <div
-            className="center"
             key={i}
+            className="UnsortedDivs"
             style={{
-              textAlign: "center",
-              color: "white",
               height: `${h}px`,
-              width: "100px",
-              margin: "5px",
               backgroundColor: `rgb(${Math.floor(
                 Math.random() * 255
               )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
@@ -49,86 +51,22 @@ function App() {
     setArr(newArr);
   }
 
-  /*   function sort() {
-    let n = arr.length;
-    let tempCont = cont;
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        if (arr[j] > arr[i]) {
-          let temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-
-          let divTemp = tempCont[i];
-          tempCont[i] = tempCont[j];
-          tempCont[j] = divTemp;
-        }
-      }
-    }
-
-    for (let i = 0; i < n; i++) {
-      console.log(`${arr[i]} ,`);
-    }
-
-    let newCont = [];
-    for (let i = 0; i < n; i++) {
-      newCont.push(
-        <div
-          className="center"
-          key={i}
-          style={{
-            height: `${arr[i]}px`,
-            width: "20px",
-            margin: "5px",
-            backgroundColor: `rgb(${Math.floor(
-              Math.random() * 255
-            )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
-              Math.random() * 255
-            )})`,
-          }}
-        ></div>
-      );
-    }
-
-    setCont(newCont);
-  } */
-
-  function sort() {
-    let n = arr.length;
+  async function sort() {
+    let tempArr = [...arr];
     let tempCont = [...cont];
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        if (arr[j] > arr[i]) {
-          let temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
+    for (let i = 0; i < tempArr.length; i++) {
+      for (let j = i; j < tempArr.length; j++) {
+        if (tempArr[i] > tempArr[j]) {
+          [tempArr[i], tempArr[j]] = [tempArr[j], tempArr[i]];
+          [tempCont[i], tempCont[j]] = [tempCont[j], tempCont[i]];
 
-          let divTemp = tempCont[i];
-          tempCont[i] = tempCont[j];
-          tempCont[j] = divTemp;
-          console.log("Doing");
-          console.log(divTemp);
+          setArr([...tempArr]);
+          setCont([...tempCont]);
+
+          await new Promise((resolve) => setTimeout(resolve, 100));
         }
       }
     }
-    setCont(tempCont);
-  }
-
-  function randomSwapping() {
-    let newCont = [...cont];
-    let sz = newCont.length;
-    for (let j = 0; j < 10; j++) {
-      for (let i = 0; i < sz; i++) {
-        let a = Math.floor(Math.random() * sz);
-        let b = Math.floor(Math.random() * sz);
-
-        let temp = newCont[a];
-        newCont[a] = newCont[b];
-        newCont[b] = temp;
-      }
-    }
-
-    setCont(newCont);
   }
 
   useEffect(() => {
